5bf0d45af14ddda5047056ba6d04f302b7461803
Ben Greene <bgree052@gold.ac.uk>
2016-10-29 20:14:53 +0100

    First Commit
    Added Unity Project

---
+++ Roll a Ball/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	public GameObject TLenemy;
+
+	public GameObject BLenemy;
+
+	public GameObject BRenemy;
+
+	public GameObject TRenemy;
+
+	// Use this for initialization
+	void Start () 
+	{
+		TLenemy = GameObject.Find ("TopLeftEnemy");
+		BLenemy	= GameObject.Find ("BottomLeftEnemy");
+		BRenemy = GameObject.Find ("BottomRightEnemy");
+		TRenemy = GameObject.Find ("TopRightEnemy");
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		transform.localPosition += transform.forward * speed * Time.deltaTime;
+	}
+
+	// Update is called once per frame
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject = TLenemy)
+		{
+			
+		}
+	}
+}
